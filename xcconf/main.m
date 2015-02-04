ba093450f6912a4fbae6f299afc00444a03b9ce3
AlexDenisov <1101.debian@gmail.com>
2015-02-04 22:51:22 +0100

    Initial commit

---
+++ xcconf/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  xcconf
+//
+//  Created by AlexDenisov on 04/02/15.
+//  Copyright (c) 2015 AlexDenisov. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
